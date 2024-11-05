401d9a7c72b5e00544caea1525f4bd347531f1cc
Nguyễn Nhật Khoa <103474813+nhatkhoa0502@users.noreply.github.com>
2024-11-05 11:17:55 +0700

    modified: complete all

--- src/App.vue
+++ src/App.vue
@@ -15,34 +15,11 @@
   </v-app>
 </template>
 <script setup>
-import {
-  computed,
-  // onMounted
-} from "vue";
+import { computed, ref } from "vue";
 import NavigationDrawer from "./components/NavigationDrawer.vue";
 import { useStore } from "vuex";
 
-// import { getRedirectResult, getAuth } from "firebase/auth";
-// import router from "@/router";
-
 const store = useStore();
 const user = computed(() => store.getters.getUser);
-
-// onMounted(async () => {
-//   const auth = getAuth();
-//   try {
-//     console.log("getRedirectResult");
-//     const result = await getRedirectResult(auth);
-//     if (result) {
-//       // Lấy thông tin người dùng từ result.user
-//       console.log("User logged in with Google", result.user);
-
-//       // Tiếp tục xử lý sau khi đăng nhập thành công
-//       router.push("/");
-//     }
-//   } catch (error) {
-//     console.error("Error during Google sign-in:", error);
-//   }
-// });
 </script>
 <style scoped></style>
